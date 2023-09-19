576cc7b7ead1b5ed6a598d090fab873210c34ef7
BBC-Esq <bbc@chintellalaw.com>
2023-09-19 10:17:58 -0400

    version 1.4.4
    Pursuant to someone who found a bug, changed the GUI coloring as well as moved the buttons and search box to the bottom.  All good suggestions.

--- gui.py
+++ gui.py
@@ -12,6 +12,7 @@ import platform
 
 LABEL_FONT = ("Segoe UI Semibold", 12)
 
+
 def determine_compute_device():
     if torch.cuda.is_available():
         COMPUTE_DEVICE = "cuda"
@@ -25,10 +26,11 @@ def determine_compute_device():
 
     config_data['COMPUTE_DEVICE'] = COMPUTE_DEVICE
 
-    with open("config.yaml", 'w') as stream:  
+    with open("config.yaml", 'w') as stream:
         yaml.safe_dump(config_data, stream)
-    
+
     return COMPUTE_DEVICE
+
 
 def is_nvidia_gpu():
     if torch.cuda.is_available():
@@ -36,91 +38,107 @@ def is_nvidia_gpu():
         return "nvidia" in gpu_name.lower()
     return False
 
+
 class DocQA_GUI:
     def __init__(self, root):
         self.root = root
 
-        main_pane = tk.PanedWindow(root, orient=tk.HORIZONTAL)
+        main_pane = tk.PanedWindow(root, orient=tk.HORIZONTAL, bg="#202123")
         main_pane.pack(fill=tk.BOTH, expand=1)
 
         # LEFT FRAME
-        left_frame = tk.Frame(main_pane)
-        
-        # 1. BUTTONS
-        self.download_embedding_model_button = tk.Button(left_frame, text="Download Embedding Model", width=26)
-        self.download_embedding_model_button.pack(pady=5)
+        left_frame = tk.Frame(main_pane, bg="#202123", highlightthickness=0, relief='flat', borderwidth=0)
 
-        self.select_embedding_model_button = tk.Button(left_frame, text="Select Embedding Model Directory", width=26)
-        self.select_embedding_model_button.pack(pady=5)
-
-        self.choose_documents_button = tk.Button(left_frame, text="Choose Documents for Database", width=26)
-        self.choose_documents_button.pack(pady=5)
-
-        self.create_chromadb_button = tk.Button(left_frame, text="Create Vector Database", width=26)
-        self.create_chromadb_button.pack(pady=5)
-
-        # 2. TABLE
+        # 1. TABLE
         create_table(left_frame)
 
-        # 3. PRO TIP
+        # 2. PRO TIP
         create_pro_tip(left_frame)
 
-        # 4. METRICS
-        self.gpu_info_label = tk.Label(left_frame, font=LABEL_FONT, foreground='green')
+        # 3. METRICS
+        self.gpu_info_label = tk.Label(
+            left_frame, font=LABEL_FONT, bg="#202123", fg='#00c600')
         self.gpu_info_label.pack(pady=0, padx=1)
 
-        self.vram_info_label = tk.Label(left_frame, font=LABEL_FONT, foreground='green')
+        self.vram_info_label = tk.Label(
+            left_frame, font=LABEL_FONT, bg="#202123", fg='violet red')
         self.vram_info_label.pack(pady=0, padx=1)
 
-        self.ram_usage_label = tk.Label(left_frame, font=LABEL_FONT, foreground='medium blue')
+        self.ram_usage_label = tk.Label(
+            left_frame, font=LABEL_FONT, bg="#202123", fg='sky blue')
         self.ram_usage_label.pack(pady=0, padx=1)
 
-        self.ram_used_label = tk.Label(left_frame, font=LABEL_FONT, foreground='medium blue') 
+        self.ram_used_label = tk.Label(
+            left_frame, font=LABEL_FONT, bg="#202123", fg='medium purple')
         self.ram_used_label.pack(pady=0, padx=1)
 
-        self.cpu_usage_label = tk.Label(left_frame, font=LABEL_FONT, foreground='violet red')
+        self.cpu_usage_label = tk.Label(
+            left_frame, font=LABEL_FONT, bg="#202123", fg='gold')
         self.cpu_usage_label.pack(pady=0, padx=1)
-        
+
         compute_device = determine_compute_device()
         os_name = platform.system().lower()
 
         if compute_device != "mps" and os_name == "windows" and is_nvidia_gpu():
-            self.cuda_logic = GPU_Monitor(self.vram_info_label, self.gpu_info_label, self.root)
-            self.system_monitor = SystemMonitor(self.cpu_usage_label, self.ram_used_label, self.ram_usage_label, self.root)
+            self.cuda_logic = GPU_Monitor(
+                self.vram_info_label, self.gpu_info_label, self.root)
+            self.system_monitor = SystemMonitor(
+                self.cpu_usage_label, self.ram_used_label, self.ram_usage_label, self.root)
         else:
             self.cuda_logic = None
             self.system_monitor = None
 
+        # 4. BUTTONS
+        self.download_embedding_model_button = tk.Button(
+            left_frame, text="Download Embedding Model", width=29, bg="#323842", fg="light gray", font=("Segoe UI Historic", 10))
+        self.download_embedding_model_button.pack(pady=2)
+
+        self.select_embedding_model_button = tk.Button(
+            left_frame, text="Select Embedding Model Directory", width=29, bg="#323842", fg="light gray", font=("Segoe UI Historic", 10))
+        self.select_embedding_model_button.pack(pady=2)
+
+        self.choose_documents_button = tk.Button(
+            left_frame, text="Choose Documents for Database", width=29, bg="#323842", fg="light gray", font=("Segoe UI Historic", 10))
+        self.choose_documents_button.pack(pady=2)
+
+        self.create_chromadb_button = tk.Button(
+            left_frame, text="Create Vector Database", width=29, bg="#323842", fg="light gray", font=("Segoe UI Historic", 10))
+        self.create_chromadb_button.pack(pady=2)
         main_pane.add(left_frame)
 
         # RIGHT FRAME
-        right_frame = tk.Frame(main_pane)
+        right_frame = tk.Frame(main_pane, bg="#202123", highlightthickness=0, relief='flat', borderwidth=0)
         main_pane.add(right_frame)
 
-        middle_frame = tk.Frame(right_frame)
+        # For displaying the answer
+        middle_frame = tk.Frame(right_frame, bg="#1e263c", highlightthickness=0, relief='flat', borderwidth=0)
         middle_frame.pack(pady=5, fill=tk.BOTH, expand=1)
 
-        self.text_input = tk.Text(middle_frame, wrap=tk.WORD, height=5)
-        self.text_input.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
-        self.text_input.configure(font=("Segoe UI Historic", 10))
-
-        scroll1 = tk.Scrollbar(middle_frame, command=self.text_input.yview)
-        scroll1.pack(side=tk.RIGHT, fill=tk.Y)
-        self.text_input.config(yscrollcommand=scroll1.set)
-
-        self.submit_query_button = tk.Button(right_frame, text="Submit Question", width=15)
-        self.submit_query_button.pack(pady=5, side=tk.TOP)
-
-        bottom_frame = tk.Frame(right_frame)
-        bottom_frame.pack(pady=5, fill=tk.BOTH, expand=1)
-
-        self.read_only_text = tk.Text(bottom_frame, wrap=tk.WORD, state=tk.DISABLED)
+        self.read_only_text = tk.Text(
+            middle_frame, wrap=tk.WORD, state=tk.DISABLED, bg="#1e263c", fg="light gray")
         self.read_only_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.read_only_text.configure(font=("Segoe UI Historic", 12))
 
-        scroll2 = tk.Scrollbar(bottom_frame, command=self.read_only_text.yview)
+        scroll2 = tk.Scrollbar(
+            middle_frame, command=self.read_only_text.yview)
         scroll2.pack(side=tk.RIGHT, fill=tk.Y)
         self.read_only_text.config(yscrollcommand=scroll2.set)
+
+        # For inputting the question
+        bottom_frame = tk.Frame(right_frame)
+        bottom_frame.pack(pady=5, fill=tk.BOTH, expand=1)
+
+        self.text_input = tk.Text(bottom_frame, wrap=tk.WORD, height=5, bg="#2a2b2e", fg="light gray")
+        self.text_input.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.text_input.configure(font=("Segoe UI Historic", 13))
+
+        scroll1 = tk.Scrollbar(bottom_frame, command=self.text_input.yview)
+        scroll1.pack(side=tk.RIGHT, fill=tk.Y)
+        self.text_input.config(yscrollcommand=scroll1.set)
+
+        self.submit_query_button = tk.Button(
+            right_frame, text="Submit Question", width=29, bg="#323842", fg="light gray", font=("Segoe UI Historic", 10))
+        self.submit_query_button.pack(pady=5, side=tk.TOP)
 
     def center_window(self, root):
         root.withdraw()
@@ -133,7 +151,7 @@ class DocQA_GUI:
 
         root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
         root.deiconify()
-  
+
     def stop_and_exit(self):
         if self.cuda_logic:
             self.cuda_logic.stop_and_exit_gpu_monitor()
@@ -147,12 +165,13 @@ class DocQA_GUI:
         self.root.protocol("WM_DELETE_WINDOW", self.stop_and_exit)
         self.root.mainloop()
 
+
 if __name__ == "__main__":
     from gui_logic import DocQA_Logic
     root = tk.Tk()
-    root.title("Version 1.4.2 - www.chintellalaw.com")
-    root.geometry("825x870")
-    root.minsize(825, 870)
+    root.title("LM Studio ChromaDB Plugin - www.chintellalaw.com")
+    root.geometry("825x900")
+    root.minsize(850, 950)
 
     app = DocQA_GUI(root)
     logic = DocQA_Logic(app)
--- gui_table.py
+++ gui_table.py
@@ -13,27 +13,27 @@ def create_table(parent_frame):
         "18.1GB", "6.8GB", "4.6GB", "2.7GB", "1.6GB", "1.6GB"
     ]
 
-    table_frame = tk.Frame(parent_frame)
+    table_frame = tk.Frame(parent_frame, bg="#202123")
     table_frame.pack(pady=5, fill=tk.BOTH, expand=1)
 
-    tk.Label(table_frame, text="Embedding Model", borderwidth=1, relief="solid").grid(row=0, column=0, sticky="nsew")
-    tk.Label(table_frame, text="Estimated VRAM", borderwidth=1, relief="solid").grid(row=0, column=1, sticky="nsew")
+    tk.Label(table_frame, text="Embedding Model", borderwidth=1, relief="solid", font=("Segoe UI Historic", 13), bg="#202123", fg="light gray").grid(row=0, column=0, sticky="nsew")
+    tk.Label(table_frame, text="Estimated VRAM", borderwidth=1, relief="solid", font=("Segoe UI Historic", 13), bg="#202123", fg="light gray").grid(row=0, column=1, sticky="nsew")
 
     for i, (model, vram) in enumerate(zip(models, vram_values), start=1):
-        tk.Label(table_frame, text=model, borderwidth=1, relief="solid").grid(row=i, column=0, sticky="nsew")
-        tk.Label(table_frame, text=vram, borderwidth=1, relief="solid").grid(row=i, column=1, sticky="nsew")
+        tk.Label(table_frame, text=model, borderwidth=1, relief="solid", font=("Segoe UI Historic", 10), bg="#202123", fg="light gray").grid(row=i, column=0, sticky="nsew")
+        tk.Label(table_frame, text=vram, borderwidth=1, relief="solid", font=("Segoe UI Historic", 10), bg="#202123", fg="light gray").grid(row=i, column=1, sticky="nsew")
 
     table_frame.grid_columnconfigure(0, weight=1)
     table_frame.grid_columnconfigure(1, weight=1)
 
 def create_pro_tip(parent_frame):
-    pro_tip_label = tk.Label(parent_frame, text="Pro tip:", font=("Segoe UI Historic", 12, "bold"))
-    pro_tip_label.pack(pady=(20, 0), anchor="w", padx=5, side=tk.TOP)
+    pro_tip_label = tk.Label(parent_frame, text="Pro tip:", font=("Segoe UI Historic", 12, "bold"), bg="#202123", fg="light gray")
+    pro_tip_label.pack(pady=(10, 0), anchor="w", padx=5, side=tk.TOP)
 
     pro_tip_text = (
         "DO NOT have LM Studio running when creating the vector database. The VRAM numbers above refer to when creating "
-        "the database, which attempts to use multithreading, hence more VRAM used. After it's created, run LM Studio and "
-        "load your LLM (remember only Llama2-based models work currently when querying the database). To query the database, "
+        "the database, hence more VRAM used. After it's created, run LM Studio and "
+        "load your LLM . To query the database, "
         "the embedding model uses much less VRAM. Use the LARGEST embedding model you can possibly fit into VRAM while the "
         "LLM is loaded into LM Studio. The quality of the embedding model is ACTUALLY MORE important than the size of the LLM. "
         "Experiment with low-quality LLMs and high-quality embedding models. For example, q3_k_3 model + instructor-xl worked "
@@ -41,5 +41,5 @@ def create_pro_tip(parent_frame):
         "strive to use as large of an embedding model as possible with as large of an LLM with the remaining VRAM."
     )
 
-    pro_tip_description = tk.Label(parent_frame, text=pro_tip_text, wraplength=400, justify="left")
+    pro_tip_description = tk.Label(parent_frame, text=pro_tip_text, wraplength=400, justify="left", font=("Segoe UI Historic", 10), bg="#202123", fg="light gray")
     pro_tip_description.pack(anchor="w", padx=5, side=tk.TOP)
