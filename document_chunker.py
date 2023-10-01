d0444170256088a884df27b76fbb70af5e2b0c4d
BBC-Esq <bbc@chintellalaw.com>
2023-10-01 12:40:39 -0400

    Delete document_chunker.py

--- document_chunker.py
+++
@@ -1,7 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def split_documents(documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=400)
-    texts = text_splitter.split_documents(documents)
-    
-    return texts
