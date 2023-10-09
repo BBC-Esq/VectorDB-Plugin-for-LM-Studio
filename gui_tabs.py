3a9f0fe5d10e5bcae31a3932ed5a8e83be88860e
BBC-Esq <bbc@chintellalaw.com>
2023-10-09 19:48:36 -0400

    v2.2

--- gui_tabs.py
+++ gui_tabs.py
@@ -1,5 +1,3 @@
-# tabs.py
-
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QTextEdit, QTabWidget
 from PySide6.QtCore import QUrl
@@ -13,10 +11,17 @@ def create_tabs(tabs_config):
     for i, tab in enumerate(tabs_config):
         tab_widget.addTab(tab_widgets[i], tab.get('name', ''))
 
+    # Adding the Tutorial tab
     tutorial_tab = QWebEngineView()
-    tab_widget.addTab(tutorial_tab, 'Tutorial')
+    tab_widget.addTab(tutorial_tab, 'Floating Point Formats')
     user_manual_folder = os.path.join(os.path.dirname(__file__), 'User_Manual')
-    html_file_path = os.path.join(user_manual_folder, 'number_format.html')
-    tutorial_tab.setUrl(QUrl.fromLocalFile(html_file_path))
+    tutorial_html_path = os.path.join(user_manual_folder, 'number_format.html')
+    tutorial_tab.setUrl(QUrl.fromLocalFile(tutorial_html_path))
+
+    # Adding the Whisper tab
+    whisper_tab = QWebEngineView()
+    whisper_html_path = os.path.join(user_manual_folder, 'whisper_quants.html')
+    whisper_tab.setUrl(QUrl.fromLocalFile(whisper_html_path))
+    tab_widget.addTab(whisper_tab, 'Whisper')
 
     return tab_widget
