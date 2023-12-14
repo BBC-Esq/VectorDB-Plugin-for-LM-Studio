b81f4c7b1ec4ff681d125b92219eddd9ae4d63a3
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2023-12-14 19:51:02 +0100

    Disable submit button until streaming is completed

--- src/gui_threads.py
+++ src/gui_threads.py
@@ -11,11 +11,14 @@ class CreateDatabaseThread(QThread):
 class SubmitButtonThread(QThread):
     responseSignal = Signal(str)
 
-    def __init__(self, user_question, parent=None):
+    def __init__(self, user_question, parent=None, callback=None):
         super(SubmitButtonThread, self).__init__(parent)
         self.user_question = user_question
+        self.callback = callback
 
     def run(self):
         response = server_connector.ask_local_chatgpt(self.user_question)
         for response_chunk in response:
             self.responseSignal.emit(response_chunk)
+        if self.callback:
+            self.callback()
