f754a8eccb41442e35b08d2cc62f21b5b07a9117
Cornelius Donley <donley@live.com>
2023-12-20 09:04:35 -0600

    Update choose_documents.py
    Check OS before opening document directory.

--- src/choose_documents.py
+++ src/choose_documents.py
@@ -1,13 +1,17 @@
 import os
+import sys
 import subprocess
 from PySide6.QtWidgets import QApplication, QFileDialog
+
 
 def choose_documents_directory():
     current_dir = os.path.dirname(os.path.realpath(__file__))
     docs_folder = os.path.join(current_dir, "Docs_for_DB")
     file_dialog = QFileDialog()
     file_dialog.setFileMode(QFileDialog.ExistingFiles)
-    file_paths, _ = file_dialog.getOpenFileNames(None, "Choose Documents for Database", current_dir)
+    file_paths, _ = file_dialog.getOpenFileNames(
+        None, "Choose Documents for Database", current_dir
+    )
 
     if file_paths:
         if not os.path.exists(docs_folder):
@@ -17,6 +21,7 @@ def choose_documents_directory():
             symlink_target = os.path.join(docs_folder, os.path.basename(file_path))
             os.symlink(file_path, symlink_target)
 
+
 def see_documents_directory():
     current_dir = os.path.dirname(os.path.realpath(__file__))
     docs_folder = os.path.join(current_dir, "Docs_for_DB")
@@ -25,10 +30,15 @@ def see_documents_directory():
     if not os.path.exists(docs_folder):
         os.mkdir(docs_folder)
 
-    # Open the directory in Windows File Explorer
-    subprocess.Popen(f'explorer "{docs_folder}"')
+    if sys.platform == "win32":
+        # Open the directory in Windows File Explorer
+        subprocess.Popen(f'explorer "{docs_folder}"')
+    elif sys.platform == "darwin":
+        # Open the directory in MacOS Finder
+        subprocess.Popen(f'open "{docs_folder}"', shell=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication([])
     choose_documents_directory()
     app.exec()
