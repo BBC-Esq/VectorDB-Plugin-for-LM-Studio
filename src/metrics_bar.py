6fbba5f0ab7b6df3f6c68dbb67586d6f9217b946
Cornelius Donley <admin@tuesdaycrowd.com>
2023-12-20 20:44:33 -0600

    check for nil handle

--- src/metrics_bar.py
+++ src/metrics_bar.py
@@ -8,14 +8,19 @@ import pynvml
 import torch
 import platform
 
+
 def is_nvidia_gpu_available():
-    return torch.cuda.is_available() and "nvidia" in torch.cuda.get_device_name(0).lower()
+    return (
+        torch.cuda.is_available() and "nvidia" in torch.cuda.get_device_name(0).lower()
+    )
+
 
 if is_nvidia_gpu_available():
     pynvml.nvmlInit()
     handle = pynvml.nvmlDeviceGetHandleByIndex(0)
 else:
     handle = None
+
 
 class MetricsCollector(QThread):
     metrics_updated = Signal(tuple)
@@ -26,9 +31,19 @@ class MetricsCollector(QThread):
             ram_usage_percent, _ = collect_ram_metrics()
             gpu_utilization, vram_usage_percent = collect_gpu_metrics(handle)
             power_usage_percent, power_limit_percent = collect_power_metrics(handle)
-            
-            self.metrics_updated.emit((cpu_usage, ram_usage_percent, gpu_utilization, vram_usage_percent, power_usage_percent, power_limit_percent))
-            time.sleep(0.5) # Update frequency
+
+            self.metrics_updated.emit(
+                (
+                    cpu_usage,
+                    ram_usage_percent,
+                    gpu_utilization,
+                    vram_usage_percent,
+                    power_usage_percent,
+                    power_limit_percent,
+                )
+            )
+            time.sleep(0.5)  # Update frequency
+
 
 class MetricsBar(QWidget):
     def __init__(self):
@@ -39,34 +54,44 @@ class MetricsBar(QWidget):
 
     def determine_compute_device(self):
         if torch.cuda.is_available():
-            return 'cuda'
+            return "cuda"
         elif torch.backends.mps.is_available():
-            return 'mps'
+            return "mps"
         else:
-            return 'cpu'
-    
+            return "cpu"
+
     def is_nvidia_gpu(self):
         if torch.cuda.is_available():
             gpu_name = torch.cuda.get_device_name(0)
             return "nvidia" in gpu_name.lower()
         return False
-    
+
     def get_os_name(self):
         return platform.system().lower()
-    
+
     def initUI(self):
         grid_layout = QGridLayout(self)
         grid_layout.setSpacing(0)
         grid_layout.setContentsMargins(0, 0, 0, 0)
 
         # CPU & RAM metrics
-        self.cpu_bar, self.cpu_percent_label = self.add_metric_to_grid("CPU Usage:", "#FF4136", grid_layout, 0)
-        self.ram_bar, self.ram_percent_label = self.add_metric_to_grid("RAM Usage:", "#B10DC9", grid_layout, 1)
+        self.cpu_bar, self.cpu_percent_label = self.add_metric_to_grid(
+            "CPU Usage:", "#FF4136", grid_layout, 0
+        )
+        self.ram_bar, self.ram_percent_label = self.add_metric_to_grid(
+            "RAM Usage:", "#B10DC9", grid_layout, 1
+        )
 
-        if self.is_nvidia_gpu(): # GPU metrics if Nvidia GPU
-            self.gpu_bar, self.gpu_percent_label = self.add_metric_to_grid("GPU Usage:", "#0074D9", grid_layout, 2)
-            self.vram_bar, self.vram_percent_label = self.add_metric_to_grid("VRAM Usage:", "#2ECC40", grid_layout, 3)
-            self.power_bar, self.power_percent_label = self.add_metric_to_grid("GPU Power:", "#FFD700", grid_layout, 4)
+        if self.is_nvidia_gpu():  # GPU metrics if Nvidia GPU
+            self.gpu_bar, self.gpu_percent_label = self.add_metric_to_grid(
+                "GPU Usage:", "#0074D9", grid_layout, 2
+            )
+            self.vram_bar, self.vram_percent_label = self.add_metric_to_grid(
+                "VRAM Usage:", "#2ECC40", grid_layout, 3
+            )
+            self.power_bar, self.power_percent_label = self.add_metric_to_grid(
+                "GPU Power:", "#FFD700", grid_layout, 4
+            )
 
     def add_metric_to_grid(self, label_text, color, grid_layout, row):
         label = QLabel(label_text)
@@ -92,23 +117,36 @@ class MetricsBar(QWidget):
         return bar
 
     def update_metrics(self, metrics):
-        cpu_usage, ram_usage_percent, gpu_utilization, vram_usage_percent, power_usage_percent, power_limit_percent = metrics
+        (
+            cpu_usage,
+            ram_usage_percent,
+            gpu_utilization,
+            vram_usage_percent,
+            power_usage_percent,
+            power_limit_percent,
+        ) = metrics
 
         self.cpu_buffer.append(cpu_usage)
         self.ram_buffer.append(ram_usage_percent)
-        
-        if self.is_nvidia_gpu(): # Update GPU metrics if Nvidia GPU
+
+        if self.is_nvidia_gpu():  # Update GPU metrics if Nvidia GPU
             self.gpu_buffer.append(gpu_utilization)
             self.vram_buffer.append(vram_usage_percent)
             self.power_buffer.append(power_usage_percent)
 
         self.update_progress_bar(self.cpu_bar, self.cpu_buffer, self.cpu_percent_label)
         self.update_progress_bar(self.ram_bar, self.ram_buffer, self.ram_percent_label)
-        
-        if self.is_nvidia_gpu(): # Update GPU metrics if Nvidia GPU
-            self.update_progress_bar(self.gpu_bar, self.gpu_buffer, self.gpu_percent_label)
-            self.update_progress_bar(self.vram_bar, self.vram_buffer, self.vram_percent_label)
-            self.update_progress_bar(self.power_bar, self.power_buffer, self.power_percent_label)
+
+        if self.is_nvidia_gpu():  # Update GPU metrics if Nvidia GPU
+            self.update_progress_bar(
+                self.gpu_bar, self.gpu_buffer, self.gpu_percent_label
+            )
+            self.update_progress_bar(
+                self.vram_bar, self.vram_buffer, self.vram_percent_label
+            )
+            self.update_progress_bar(
+                self.power_bar, self.power_buffer, self.power_percent_label
+            )
 
     def update_progress_bar(self, bar, buffer, label):
         if buffer:
@@ -129,26 +167,38 @@ class MetricsBar(QWidget):
         self.metrics_collector.start()
 
     def stop_metrics_collector(self):
-        if hasattr(self, 'metrics_collector'):
+        if hasattr(self, "metrics_collector"):
             self.metrics_collector.terminate()
             self.metrics_collector.wait()
+
 
 def collect_cpu_metrics():
     percentages = psutil.cpu_percent(interval=0.1, percpu=True)
     return sum(percentages) / len(percentages)
 
+
 def collect_ram_metrics():
     ram = psutil.virtual_memory()
     return ram.percent, ram.used
 
+
 def collect_gpu_metrics(handle):
+    if handle == None:
+        return 0, 0
+
     memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
     gpu_utilization = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
 
-    vram_usage_percent = (memory_info.used / memory_info.total) * 100 if memory_info.total > 0 else 0
+    vram_usage_percent = (
+        (memory_info.used / memory_info.total) * 100 if memory_info.total > 0 else 0
+    )
     return gpu_utilization, vram_usage_percent
 
+
 def collect_power_metrics(handle):
+    if handle == None:
+        return 0, 0
+
     power_usage = pynvml.nvmlDeviceGetPowerUsage(handle) / 1000.0
     power_limit = pynvml.nvmlDeviceGetPowerManagementLimit(handle) / 1000.0
     power_percentage = (power_usage / power_limit) * 100 if power_limit > 0 else 0
