702d130e857a12fef8c526f4e2f84e345edb6a63
BBC-Esq <bbc@chintellalaw.com>
2023-12-21 21:57:48 -0500

    v2.7.4
    Address concerns by https://github.com/BBC-Esq/ChromaDB-Plugin-for-LM-Studio/pull/53/commits/f754a8eccb41442e35b08d2cc62f21b5b07a9117 while additionally address linux users and the fact that prior to the pull request I had already refactored to us Pathlib instead.

--- src/choose_documents.py
+++ src/choose_documents.py
@@ -1,34 +1,35 @@
-import os
 import subprocess
+import os
+from pathlib import Path
 from PySide6.QtWidgets import QApplication, QFileDialog
 
 def choose_documents_directory():
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    docs_folder = os.path.join(current_dir, "Docs_for_DB")
+    current_dir = Path(__file__).parent.resolve()
+    docs_folder = current_dir / "Docs_for_DB"
     file_dialog = QFileDialog()
     file_dialog.setFileMode(QFileDialog.ExistingFiles)
-    file_paths, _ = file_dialog.getOpenFileNames(None, "Choose Documents for Database", current_dir)
+    file_paths, _ = file_dialog.getOpenFileNames(None, "Choose Documents for Database", str(current_dir))
 
     if file_paths:
-        if not os.path.exists(docs_folder):
-            os.mkdir(docs_folder)
+        docs_folder.mkdir(parents=True, exist_ok=True)
 
         for file_path in file_paths:
-            symlink_target = os.path.join(docs_folder, os.path.basename(file_path))
-            os.symlink(file_path, symlink_target)
+            symlink_target = docs_folder / Path(file_path).name
+            symlink_target.symlink_to(file_path)
 
 def see_documents_directory():
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    docs_folder = os.path.join(current_dir, "Docs_for_DB")
+    current_dir = Path(__file__).parent.resolve()
+    docs_folder = current_dir / "Docs_for_DB"
 
-    # Ensure the directory exists
-    if not os.path.exists(docs_folder):
-        os.mkdir(docs_folder)
+    docs_folder.mkdir(parents=True, exist_ok=True)
 
-    # Open the directory in Windows File Explorer
-    subprocess.Popen(f'explorer "{docs_folder}"')
+    # Cross-platform directory opening
+    if os.name == 'nt':  # Windows
+        subprocess.Popen(f'explorer "{str(docs_folder)}"')
+    elif os.name == 'posix':  # MacOS and Linux
+        subprocess.Popen(f'xdg-open "{str(docs_folder)}"')
 
 if __name__ == '__main__':
     app = QApplication([])
     choose_documents_directory()
-    app.exec()
+    app.exec_()
