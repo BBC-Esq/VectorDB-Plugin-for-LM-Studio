fd52968d4c0d2cf51057a68feb8d617eb20fc70f
BBC-Esq <bbc@chintellalaw.com>
2024-02-21 14:13:05 -0500

    openai 1.12.0 compatible

--- src/server_connector.py
+++ src/server_connector.py
@@ -1,5 +1,5 @@
 import os
-import openai
+from openai import OpenAI
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings, HuggingFaceBgeEmbeddings
 from chromadb.config import Settings
@@ -7,8 +7,6 @@ import yaml
 import torch
 from transformers import AutoTokenizer
 import gc
-import tempfile
-import subprocess
 from pathlib import Path
 from PySide6.QtWidgets import QMessageBox
 import sys
@@ -39,13 +37,11 @@ def format_metadata_as_citations(metadata_list):
 
 def write_contexts_to_file_and_open(contexts, metadata_list):
     contexts_output_file_path = Path('contexts.txt')
-
     with contexts_output_file_path.open('w', encoding='utf-8') as file:
         for index, (context, metadata) in enumerate(zip(contexts, metadata_list), start=1):
             file_name = metadata.get('file_name')
             file.write(f"---------- Context {index} | From File: {file_name} ----------\n\n")
             file.write(context + "\n\n")
-    
     if os.name == 'nt':
         os.startfile(contexts_output_file_path)
     elif sys.platform == 'darwin':
@@ -57,87 +53,50 @@ def write_contexts_to_file_and_open(contexts, metadata_list):
 
 def connect_to_local_chatgpt(prompt):
     global stop_streaming
-
     with open('config.yaml', 'r') as config_file:
         config = yaml.safe_load(config_file)
         server_config = config.get('server', {})
-        openai_api_base = server_config.get('connection_str')
-        openai_api_key = server_config.get('api_key')
-        prefix = server_config.get('prefix')
-        suffix = server_config.get('suffix')
+        base_url = server_config.get('connection_str', 'http://localhost:1234/v1')
+        prefix = server_config.get('prefix', '')
+        suffix = server_config.get('suffix', '')
         prompt_format_disabled = server_config.get('prompt_format_disabled', False)
-        model_temperature = server_config.get('model_temperature')
-        model_max_tokens = server_config.get('model_max_tokens')
+        model_temperature = server_config.get('model_temperature', 0.7)
+        model_max_tokens = server_config.get('model_max_tokens', 100)
+        model_max_tokens = server_config.get('api_key')
 
-    openai.api_base = openai_api_base
-    openai.api_key = openai_api_key
+    client = OpenAI(base_url=base_url, api_key=api_key)  # API key is not needed for local connections
 
     if prompt_format_disabled:
         formatted_prompt = prompt
     else:
         formatted_prompt = f"{prefix}{prompt}{suffix}"
 
-    response = openai.ChatCompletion.create(
-        model="local model",
+    response = client.chat.completions.create(
+        model="local-model",  # Placeholder for local model identifier
+        messages=[{"role": "user", "content": formatted_prompt}],
         temperature=model_temperature,
-        max_tokens=model_max_tokens,
-        messages=[{"role": "user", "content": formatted_prompt}], stream=True
+        max_tokens=model_max_tokens
     )
-    for chunk in response:
-        if stop_streaming:
-            yield None
-            break
-        # parse chunks and yield message
-        if 'choices' in chunk and len(chunk['choices']) > 0 and 'delta' in chunk['choices'][0] and 'content' in chunk['choices'][0]['delta']:
-            chunk_message = chunk['choices'][0]['delta']['content']
-            yield chunk_message
+
+    if not stop_streaming:
+        for choice in response.choices:
+            yield choice.message.content
 
 def initialize_vector_model(EMBEDDING_MODEL_NAME, config_data, compute_device):
-    # Common encode_kwargs for all models to normalize embeddings
     common_encode_kwargs = {'normalize_embeddings': True}
-    
     if "instructor" in EMBEDDING_MODEL_NAME:
         embed_instruction = config_data['instructor'].get('embed_instruction')
         query_instruction = config_data['instructor'].get('query_instruction')
-
-        return HuggingFaceInstructEmbeddings(
-            model_name=EMBEDDING_MODEL_NAME,
-            model_kwargs={"device": compute_device},
-            embed_instruction=embed_instruction,
-            query_instruction=query_instruction,
-            encode_kwargs={'normalize_embeddings': True}
-        )
-
+        return HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": compute_device}, embed_instruction=embed_instruction, query_instruction=query_instruction, encode_kwargs=common_encode_kwargs)
     elif "bge" in EMBEDDING_MODEL_NAME:
         query_instruction = config_data['bge'].get('query_instruction')
-
-        return HuggingFaceBgeEmbeddings(
-            model_name=EMBEDDING_MODEL_NAME,
-            model_kwargs={"device": compute_device},
-            query_instruction=query_instruction,
-            encode_kwargs={'normalize_embeddings': True}
-        )
-
+        return HuggingFaceBgeEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": compute_device}, query_instruction=query_instruction, encode_kwargs=common_encode_kwargs)
     else:
-        return HuggingFaceEmbeddings(
-            model_name=EMBEDDING_MODEL_NAME,
-            model_kwargs={"device": compute_device},
-            encode_kwargs={'normalize_embeddings': True}
-        )
-        
+        return HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": compute_device}, encode_kwargs=common_encode_kwargs)
+
 def initialize_database(embeddings, persist_directory, client_settings, score_threshold, k, search_filter):
-    db = Chroma(
-        persist_directory=persist_directory,
-        embedding_function=embeddings,
-        client_settings=client_settings,
-    )
-
-    retriever = db.as_retriever(search_kwargs={
-        'score_threshold': score_threshold, 
-        'k': k,
-        'filter': search_filter
-    })
-
+    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=client_settings)
+    retriever = db.as_retriever(search_kwargs={'score_threshold': score_threshold, 'k': k, 'filter': search_filter})
     return db, retriever
 
 def retrieve_and_filter_contexts(query, config, embeddings, db):
@@ -145,82 +104,43 @@ def retrieve_and_filter_contexts(query, config, embeddings, db):
     document_types = config['database'].get('document_types', '')
     score_threshold = float(config['database']['similarity'])
     k = int(config['database']['contexts'])
-
-    if document_types:
-        search_filter = {'document_type': document_types}
-    else:
-        search_filter = {}
-
-    retriever = db.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={
-            'score_threshold': score_threshold, 
-            'k': k,
-            'filter': search_filter
-        }
-    )
-
+    search_filter = {'document_type': document_types} if document_types else {}
+    retriever = db.as_retriever(search_kwargs={'score_threshold': score_threshold, 'k': k, 'filter': search_filter})
     my_cprint("Querying database.", "white")
     relevant_contexts = retriever.get_relevant_documents(query)
-
     filtered_contexts = [doc for doc in relevant_contexts if search_term in doc.page_content.lower()]
-
     if not filtered_contexts:
         my_cprint("No relevant contexts/chunks found.  Make sure the following settings are not too restrictive: (1) Similarity, (2) Contexts, and (3) Search Filter", "yellow")
-
     return filtered_contexts
 
 def ask_local_chatgpt(query, chunks_only, persist_directory=str(PERSIST_DIRECTORY), client_settings=CHROMA_SETTINGS):
     global stop_streaming
     stop_streaming = False
     my_cprint("Attempting to connect to server.", "white")
-
     with open('config.yaml', 'r') as config_file:
         config = yaml.safe_load(config_file)
-    
     EMBEDDING_MODEL_NAME = config['EMBEDDING_MODEL_NAME']
     compute_device = config['Compute_Device']['database_query']
     config_data = config.get('embedding-models', {})
-
     embeddings = initialize_vector_model(EMBEDDING_MODEL_NAME, config_data, compute_device)
     my_cprint("Embedding model loaded.", "green")
-
     tokenizer_path = "./Tokenizer"
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-
-    # Initialize Database
-    db = Chroma(
-        persist_directory=persist_directory,
-        embedding_function=embeddings,
-        client_settings=client_settings,
-    )
+    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=client_settings)
     my_cprint("Database initialized.", "white")
-
-    # Retrieve and filter contexts
     filtered_contexts = retrieve_and_filter_contexts(query, config, embeddings, db)
-
     contexts = [document.page_content for document in filtered_contexts]
     metadata_list = [document.metadata for document in filtered_contexts]
-
-    # Save metadata to file
     save_metadata_to_file(metadata_list, metadata_output_file_path)
-
     if chunks_only:
         write_contexts_to_file_and_open(contexts, metadata_list)
         return {"answer": "Contexts written to temporary file and opened", "sources": filtered_contexts}
-
-    # Prepare the augmented query
     prepend_string = "Only base your answer to the following question on the provided context/contexts accompanying this question. If you cannot answer based on the included context/contexts alone, please state so."
     augmented_query = prepend_string + "\n\n---\n\n".join(contexts) + "\n\n-----\n\n" + query
-
     my_cprint(f"Number of relevant contexts: {len(filtered_contexts)}", "white")
-
     total_tokens = sum(len(tokenizer.encode(context)) for context in contexts)
     my_cprint(f"Total number of tokens in contexts: {total_tokens}", "white")
-
-    # Connect to local ChatGPT and process response
     response_json = connect_to_local_chatgpt(augmented_query)
-
     full_response = []
     for chunk_message in response_json:
         if chunk_message is None:
@@ -229,37 +149,29 @@ def ask_local_chatgpt(query, chunks_only, persist_directory=str(PERSIST_DIRECTOR
             full_response[-1] += chunk_message
         else:
             full_response.append(chunk_message)
-
         yield chunk_message
-
-    # Write chat history
     chat_history_file_path = ROOT_DIRECTORY / 'chat_history.txt'
     with chat_history_file_path.open('w', encoding='utf-8') as file:
         for message in full_response:
             file.write(message)
-
     yield "\n\n"
-
-    # Generate and yield citations
     citations = format_metadata_as_citations(metadata_list)
     unique_citations = []
     for citation in citations.split("\n"):
         if citation not in unique_citations:
             unique_citations.append(citation)
-    
     yield "\n".join(unique_citations)
-
     del embeddings.client
     del embeddings
     torch.cuda.empty_cache()
     gc.collect()
     my_cprint("Embedding model removed from memory.", "red")
-
     return {"answer": response_json, "sources": filtered_contexts}
 
 def stop_interaction():
     global stop_streaming
     stop_streaming = True
+
 
 ''' Search by metadata and minimum relevance score threshold
 
